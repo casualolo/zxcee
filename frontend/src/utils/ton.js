0bb43f6fe285f6a28fa1fb321901a55d54313aad
casualolo <kirill15.07.1a@gmail.com>
2026-07-21 03:49:24 +0300

    Delete frontend directory

--- frontend/src/utils/ton.js
+++
@@ -1,6 +0,0 @@
-export const TONCONNECT_MANIFEST_URL = `${window.location.origin}/tonconnect-manifest.json`
-
-export function formatTonAddress(address, chars = 4) {
-  if (!address) return ''
-  return `${address.slice(0, chars)}...${address.slice(-chars)}`
-}
